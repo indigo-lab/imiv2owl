d16d5805f3b5b39c810f4ba09bfc84b8b7ba94a2
yuzo <yuzo@indigo.co.jp>
2018-12-11 16:01:19 +0900

    Initial commit

--- bin/imiv2owl.js
+++ bin/imiv2owl.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
-if (process.argv.length < 3) {
-  console.error("usage: imiv2owl [path_to_imiv]");
-  process.exit(1);
-}
-
+const readline = require('readline');
 const fs = require('fs');
 const imiv2owl = require('../imiv2owl');
-const result = imiv2owl(fs.readFileSync(process.argv[2], "UTF-8"));
-console.log(JSON.stringify(result, null, 2));
+
+const rl = readline.createInterface({
+  input: process.argv.length < 3 ? process.stdin : fs.createReadStream(process.argv[2], 'UTF-8'),
+  crlfDelay: Infinity
+});
+
+const lines = [];
+
+rl.on('line', (line) => {
+  lines.push(line);
+}).on('close', () => {
+  const result = imiv2owl(lines.join('\n'));
+  console.log(JSON.stringify(result, null, 2));
+});
